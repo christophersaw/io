d8d214837109a60ef5c54b87cc2db66b3e89782d
christophersaw <93069961+christophersaw@users.noreply.github.com>
2021-10-31 21:56:44 -0700

    Add files via upload

--- blp_algo_v2.py
+++ blp_algo_v2.py
@@ -10,6 +10,8 @@ os.chdir("/Users/christophersaw/Desktop/blp")
 data=pd.read_csv(r'headache.csv')
 data=data.sort_values(by=['store','week','brand'])
 
+# PART ONE: DEMAND ESTIMATION
+
 # No. of individuals (simulations), products (brands) and markets (store-weeks)
 ns=20
 nj=11
@@ -17,38 +19,21 @@ nt=3504
 
 # PREPARE DATA
 # Market shares (nj x nt)
-shares=pd.DataFrame(data['shares'])
-shares=shares.join(data['market'])
-shares=shares.join(data['brand'])
-shares=shares.pivot(index='brand',columns='market',values='shares')
-shares=shares.to_numpy()
-
-outsideshare=pd.DataFrame(data['outsideshare'])
-outsideshare=outsideshare.join(data['market'])
-outsideshare=outsideshare.join(data['brand'])
-outsideshare=outsideshare.pivot(index='brand',columns='market',values='outsideshare')
-outsideshare=outsideshare.to_numpy()
+shares=pd.DataFrame(data['shares']).join(data['market']).join(data['brand']).pivot(index='brand',columns='market',values='shares').to_numpy()
+outsideshare=pd.DataFrame(data['outsideshare']).join(data['market']).join(data['brand']).pivot(index='brand',columns='market',values='outsideshare').to_numpy()
 
 # Prices (nj x nt)
-price=pd.DataFrame(data['price'])
-price=price.join(data['market'])
-price=price.join(data['brand'])
-price=price.pivot(index='brand',columns='market',values='price')
-price=price.to_numpy()
+price=pd.DataFrame(data['price']).join(data['market']).join(data['brand']).pivot(index='brand',columns='market',values='price').to_numpy()
 
 # Branded Product dummy
-branded=pd.DataFrame(data['branded_product'])
-branded=branded.join(data['market'])
-branded=branded.join(data['brand'])
-branded=branded.pivot(index='brand',columns='market',values='branded_product')
-branded=branded.to_numpy()
+branded=pd.DataFrame(data['branded_product']).join(data['market']).join(data['brand']).pivot(index='brand',columns='market',values='branded_product').to_numpy()
 
 # Income (nj x nt x ns)
-d=pd.read_csv(r'OTCDemographics.csv',sep='\t')
-d=d.sort_values(by=['store','week'])
+d=pd.read_csv(r'OTCDemographics.csv',sep='\t').sort_values(by=['store','week'])
 del(d['store'],d['week'])
 d=d.to_numpy()
 inc=np.array([np.repeat(i, nj).reshape(ns, nj) for i in d]).swapaxes(1,2).swapaxes(0,1)
+inc_jt=pd.DataFrame(inc[:,153,:]) # incomes for store 9 week 10
 
 # Nu (nj x nt x ns)
 v=np.random.normal(0,1,(nt,ns))
@@ -56,28 +41,20 @@ v=pd.DataFrame(v)
 v.columns=["rand_"+str(i) for i in range(1,21)]
 v=v.to_numpy()
 nu=np.array([np.repeat(i, nj).reshape(ns, nj) for i in v]).swapaxes(1,2).swapaxes(0,1)
+nu_jt=pd.DataFrame(nu[:,153,:]) # shocks for store 9 week 10
 
 # SET UP VARIABLES
 # X1: Linear parameters = prom + 11 brand dummies (nj*nt x 13)
-x1=pd.get_dummies(data['brand'])
-x1.columns=["brand_"+str(i) for i in range(1,12)]
-x1=x1.join(data['prom'])
-x1=x1.join(data['price'])
-X1=x1.to_numpy()
+X1=pd.get_dummies(data['brand'])
+X1.columns=["brand_"+str(i) for i in range(1,12)]
+X1=X1.join(data['prom']).join(data['price']).to_numpy()
 
 # X2: Non linear parameters price + branded_product (nj*nt x 2)
-x2=pd.DataFrame(data['price'])
-x2=x2.join(data['branded_product'])
-X2=x2.to_numpy()
+X2=pd.DataFrame(data['price']).join(data['branded_product']).to_numpy()
 
 # Instruments (nj*nt x 45)
-iv=pd.read_csv(r'headache_instr.csv')
-iv=iv.sort_values(by=['store','week','brand'])
+iv=pd.read_csv(r'headache_instr.csv').sort_values(by=['store','week','brand'])
 del(iv['store'],iv['week'],iv['brand'],iv['weight'])
-# iv=pd.merge(iv, x1,left_index=True,right_index=True)
-# del(iv['price'])
-# iv=pd.merge(iv, x2,left_index=True,right_index=True)
-# del(iv['price'])
 # list(iv)
 # ['cost', 'avoutprice', 
 # 'pricestore1', 'pricestore2', 'pricestore3', 'pricestore4', 'pricestore5', 
@@ -128,38 +105,99 @@ theta2=np.array([[0.001],       #sigma_b
                  [0.001]])      #sigma_i
 results=sp.optimize.minimize(gmmobjfn,theta2,method='Nelder-Mead')
 # results
-# final_simplex: (array([[-0.27854442,  0.04899133],
-#       [-0.27860998,  0.04899912],
-#       [-0.27845884,  0.04900023]]), array([11.50313971, 11.50313971, 11.50313973]))
-#           fun: 11.503139705392927
+# final_simplex: (array([[0.08765094, 0.05278045],
+#       [0.08766798, 0.05278336],
+#       [0.08755854, 0.05278304]]), array([11.56531214, 11.56531214, 11.56531214]))
+#           fun: 11.5653121401884
 #       message: 'Optimization terminated successfully.'
-#          nfev: 139
-#           nit: 72
+#          nfev: 124
+#           nit: 64
 #        status: 0
 #       success: True
-#             x: array([-0.27854442,  0.04899133])
+#             x: array([0.08765094, 0.05278045])
 theta2_soln=results.x
-delta=delta(theta2_soln)
 theta1_soln=np.linalg.inv(X1.transpose() @ Z @ omega @ Z.transpose() @ X1) @ X1.transpose() @ Z @ omega @ Z.transpose() @ delta
-theta1_soln=pd.DataFrame(theta1_soln)
-theta2_soln=pd.DataFrame(theta2_soln)
-# theta1_soln                                                                                                                                                                             
-#             0
-# 0  -12.086905
-# 1   -6.471710
-# 2  -12.487131
-# 3   -8.659687
-# 4   -5.361192
-# 5   -5.494671
-# 6  -10.602938
-# 7  -10.483532
-# 8  -11.362179
-# 9  -14.005194
-# 10  -6.816987
-# 11   0.657570
-# 12  -0.293241
-# theta2_soln
-#           0
-# 0 -0.278544
-# 1  0.048991
+pd.DataFrame(theta1_soln).to_csv('theta1.csv',index=False)
+pd.DataFrame(theta2_soln).to_csv('theta2.csv',index=False)
 
+# PART TWO: MARKET ANALYSIS
+
+# Calculate mean utility given solution for theta2
+def delta(theta2):
+    # Set problem parameters
+    x=0
+    maxiter=1000
+    diff=1
+    epsilon=1e-05
+    # Define arrays
+    num=np.zeros((nj,nt,ns))
+    den=np.zeros((nj,nt,ns)) # take average of num/den along axis 2 to calculate integral numerically
+    w_h=np.zeros((nj,nt,maxiter))
+    w_h[:,:,0]=np.exp(np.log(shares)-np.log(outsideshare)) # w_init = exp(delta_init); delta_init = log(shares)-log(outsideshare) from logit model
+    while diff > epsilon and x < maxiter:
+        delta_h=np.log(w_h[:,:,x])
+        for i in range(ns):
+            num[:,:,i]=np.exp(delta_h[:,:]+theta2[0]*nu[:,:,i]*branded[:,:]+theta2[1]*inc[:,:,i]*price[:,:])
+            den[:,:,i]=np.sum(num[j,:,i] for j in range(nj)) + 1
+        predicted_shares=np.mean(np.divide(num,den),axis=2)
+        w_h[:,:,x+1]=np.multiply(w_h[:,:,x],np.divide(shares,predicted_shares)) # w_h+1 = w_h * (shares/predicted_shares), where w = exp(delta)
+        diff=np.linalg.norm(shares - predicted_shares)
+        x=x+1
+    return np.log(w_h[:,:,x]).flatten('F').transpose()
+
+data['delta_jt']=delta(theta2_soln)
+
+# Extract data for store 9, week 10
+data2=data.loc[data['market_ids']=='9x10']
+branded=pd.DataFrame(data2['branded_product']).join(data2['market']).join(data2['brand']).pivot(index='brand',columns='market',values='branded_product').to_numpy()
+price=pd.DataFrame(data2['price']).join(data2['market']).join(data2['brand']).pivot(index='brand',columns='market',values='price').to_numpy()
+shares=pd.DataFrame(data2['shares']).join(data2['market']).join(data2['brand']).pivot(index='brand',columns='market',values='shares').to_numpy()
+inc=inc_jt.loc[0]
+nu=nu_jt.loc[0]
+
+# mu_ij (i's deviation from mean utility of product j)
+mu=np.zeros((20,11))
+for i in range(20):
+    for j in range(11):
+        mu[i,j]=theta2_soln[0]*nu[i]*branded[j] + theta2_soln[1]*inc[i]*price[j]
+
+# delta_j (mean utility of product j)
+delta=data2['delta_jt'].to_numpy()
+
+# prob_ij (probability of individual i purchasing product j)
+num=np.zeros((20,11))
+den=np.zeros((20,11))
+for i in range(20):
+    for j in range(11):
+        num[i,:]=np.exp(delta+mu[i,:])
+        den[i,:]=np.sum(num[i,k] for k in range(11)) + 1
+    prob=np.divide(num,den)
+
+# alpha
+alpha=theta1_soln[12]+theta2_soln[1]*inc
+alpha=alpha.to_numpy()
+alpha_logit=theta1_soln[12]
+
+# Cross- and own- elasticities from Random Coefficients Model
+eta=np.zeros((11,11))
+for j in range(11):
+    for k in range(11):
+        if j==k:
+            eta[j,j] = (-1)*(price[j]/shares[j])*(np.mean(alpha*prob[:,j]*(1-prob[:,j])))
+        else:
+            eta[j,k] = (price[k]/shares[j])*(np.mean(alpha*prob[:,j]*prob[:,k]))
+
+elasticities=pd.DataFrame(eta)
+elasticities.round(4).to_csv('elasticities_rc.csv')
+
+# Cross- and own- elasticities from Logit Model
+eta_logit=np.zeros((11,11))
+for j in range(11):
+    for k in range(11):
+        if j==k:
+            eta_logit[j,j] = alpha_logit*price[j]*(1-shares[j])
+        else:
+            eta_logit[j,k] = (-1)*alpha_logit*price[k]*(shares[k])
+
+elasticities_logit=pd.DataFrame(eta_logit)
+elasticities_logit.round(4).to_csv('elasticities_logit.csv')
